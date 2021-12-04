811a012e0d89c42362d0219fcb57b637d155d368
eun ji <4484jjj@naver.com>
2021-12-04 23:46:19 +0900

    가맹점 등록

--- app/src/main/java/com/example/lifeplus/RegistrationActivity.java
+++ app/src/main/java/com/example/lifeplus/RegistrationActivity.java
@@ -14,61 +14,10 @@ import java.io.InputStream;
 
 
 public class RegistrationActivity extends AppCompatActivity {
-    // 갤러리에서 사진 가져오기
-    private static final int REQUEST_CODE=0;
-    private ImageView imageView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
-        imageView = findViewById(R.id.imageView2);
-        imageView.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-               startActivityForResult(intent, REQUEST_CODE,null);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                try {
-                    InputStream in = getContentResolver().openInputStream(data.getData());
-
-                    Bitmap img = BitmapFactory.decodeStream(in);
-                    in.close();
-
-                    imageView.setImageBitmap(img);
-                } catch (Exception e) {
-
-                }
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
-            }
         }
-    }
-
-    /*public void upload_bt(View v){
-        Toast.makeText(this, "정보 등록 성공", Toast.LENGTH_SHORT).show();
-    }*/
-
-
-    // 사진찍기 기능(카메라)
-    /*static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }*/
-
 }
