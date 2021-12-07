9ba1c87922f0fbaae12c9c51c116a826f3c876dc
eun ji <4484jjj@naver.com>
2021-12-07 20:11:30 +0900

    파이어 베이스 연결

---
+++ app/src/main/java/com/example/lifeplus/User.java
@@ -0,0 +1,43 @@
+package com.example.lifeplus;
+
+public class User {
+    public String store_name;
+    public String category;
+    public String store_address;
+
+    public User(){
+
+    }
+
+    public User(String _storename, String _storeaddress/*, String _category*/){
+        store_name = _storename;
+        store_address = _storeaddress;
+        //category = _category;
+    }
+
+    public String getStorename(){
+        return store_name;
+    }
+    public void setStorename(String _storename){
+        store_name = _storename;
+    }
+
+    public String getAddress(){
+        return store_address;
+    }
+    public void setAddress(String _storeaddress){
+        store_name = _storeaddress;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+    public void setCategory(String _category){
+        store_name = _category;
+    }
+
+    @Override
+    public String toString(){
+        return "가맹점 : "+store_name+", "+"주소 : " + store_address; //+", "+"카테고리 : "+category
+    }
+}
